37eb0fab957457261c4402a02304fceda44d4c96
AgusBaha <id.stackdevelopers@gmail.com>
2023-09-25 19:36:34 +0700

    feat(login and register) templating page login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,90 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<x-auth-layout title="Register">
+    @push('style')
+    @endpush
+    <section>
+        <div class="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-75-screen">
+            <div class="container z-10">
+                <div class="flex flex-wrap mt-0 -mx-3">
+                    <div
+                        class="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12">
+                        <div
+                            class="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
+                            <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+                                <h3
+                                    class="relative z-10 font-bold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">
+                                    Welcome!</h3>
+                                <p class="mb-0">Use these awesome forms to login or create new account in your project
+                                    for free.</p>
+                            </div>
+                            <div class="flex-auto p-6">
+                                <form role="form" method="POST" action="{{ route('register') }}">
+                                    @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                    <label class="mb-2 ml-1 font-bold text-xs text-slate-700"
+                                        for="name">Name</label>
+                                    <div class="mb-4">
+                                        <input type="text" id="name" name="name" :value="old('name')"
+                                            required autofocus autocomplete="name"
+                                            class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                            placeholder="Email" aria-label="Email" aria-describedby="email-addon" />
+                                    </div>
+                                    <label class="mb-2 ml-1 font-bold text-xs text-slate-700"
+                                        for="email">Email</label>
+                                    <div class="mb-4">
+                                        <input type="email" id="email" name="email" :value="old('email')"
+                                            required autocomplete="email"
+                                            class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                            placeholder="Email" aria-label="Email" aria-describedby="email-addon" />
+                                    </div>
+                                    <label class="mb-2 ml-1 font-bold text-xs text-slate-700"
+                                        for="password">Password</label>
+                                    <div class="mb-4">
+                                        <input type="password" id="password" name="password" required
+                                            autocomplete="new-password"
+                                            class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                            placeholder="Password" aria-label="Password"
+                                            aria-describedby="password-addon" />
+                                    </div>
+                                    <label class="mb-2 ml-1 font-bold text-xs text-slate-700"
+                                        for="password_confirmation">Confirm
+                                        Password</label>
+                                    <div class="mb-4">
+                                        <input type="password" id="password_confirmation" name="password_confirmation"
+                                            required autocomplete="new-password"
+                                            class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                                            placeholder="Password" aria-label="Password"
+                                            aria-describedby="password-addon" />
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit"
+                                            class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-blue-600 to-cyan-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">Sign
+                                            in</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div
+                                class="p-6 px-1 pt-0 text-center bg-transparent border-t-0 border-t-solid rounded-b-2xl lg:px-2">
+                                <p class="mx-auto mb-6 leading-normal text-sm">
+                                    Already registered?
+                                    <a href="{{ route('login') }}"
+                                        class="relative z-10 font-semibold text-transparent bg-gradient-to-tl from-blue-600 to-cyan-400 bg-clip-text">Sign
+                                        in</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
+                        <div
+                            class="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
+                            <div class="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover skew-x-10"
+                                style="background-image: url('{{ asset('assets/img/curved-images/curved6.jpg') }}')">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+    @push('script')
+    @endpush
+</x-auth-layout>
